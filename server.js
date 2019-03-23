ca1e4e1edf7504ca1c073e6a27ba68e9ece2d719
Luke Hazelton <tomluke600@gmail.com>
2019-03-23 16:57:33 -0400

    Setup
    Setting up the passport functionality that will allow login to work across pages

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const expenseRoutes = express.Router();
 const PORT = process.env.PORT || 4000; // "process.env.PORT" is Heroku's port if we're deploying there, then 4000 is a custom chosen port for dev testing
 const path = require("path");
 const dotenv = require("dotenv").config();
+const passport = require("passport");
+
 let Expense = require('./models/expense');
 
 var userId = '';
@@ -15,15 +17,22 @@ let User = require('./models/user');
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-
+// DB Config
+const db = require("./config/keys").mongoURI;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client", "build")))
-mongoose.connect('mongodb://Admin:Hello2@cluster0-shard-00-00-7dwwj.mongodb.net:27017,cluster0-shard-00-01-7dwwj.mongodb.net:27017,cluster0-shard-00-02-7dwwj.mongodb.net:27017/testLarge?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('db', { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
 
 // For mobile testing
 expenseRoutes.post('/all', (req, res, next) => {
