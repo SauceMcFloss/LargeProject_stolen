2276bd3a33c26800e680f6dff086b0cba7bc8f8a
Luke Hazelton <tomluke600@gmail.com>
2019-03-25 12:13:08 -0400

    DELETE/REPLACE EVERYTHING
    Starting from a different project (that I stole obviosuly)

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const expenseRoutes = express.Router();
-const PORT = process.env.PORT || 4000; // "process.env.PORT" is Heroku's port if we're deploying there, then 4000 is a custom chosen port for dev testing
-const path = require("path");
-const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const passport = require("passport");
 
-let Expense = require('./models/expense');
+const users = require("./routes/api/users");
 
-var userId = '';
+const app = express();
 
-let User = require('./models/user');
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "client", "build")))
-mongoose.connect(db, { useNewUrlParser: true });
-const connection = mongoose.connection;
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-})
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -34,233 +33,9 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
-// For mobile testing
-expenseRoutes.post('/all', (req, res, next) => {
-  const userId = req.body.userId;
-  Expense.find({userId: userId})
-  .select("_id description amount month day year")
-  .exec()
-  .then(docs => {
-	res.status(200).json(
-		docs.map(doc => {
-			return {
-				description: doc.description,
-				amount: doc.amount,
-				month: doc.month,
-				day: doc.day,
-				year: doc.year
-			}
-		})
-	);
-  })
-  .catch(err => {
-	console.log(err);
-	res.status(500).json({
-  	  error: err
-	})
-  });
-});
-// For mobile testing
-expenseRoutes.route('/').get(function(req, res) {
-    Expense.find(function(err, expenses) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(expenses);
-        }
-    });
-});
+// Routes
+app.use("/api/users", users);
 
-// Login route
-expenseRoutes.post("/loginUser", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
+const port = process.env.PORT || 5000;
 
-  // Find user by email
-  User.findOne({ username }).then(user => {
-    // Check if user exists
-    if (!user) {
-      return res.status(404).json({ emailnotfound: "Email not found" });
-    }
-
-    // Check password
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name
-        };
-
-        // Sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ passwordincorrect: "Password incorrect" });
-      }
-    });
-  });
-});
-
-// Add new user
-expenseRoutes.post("/createUser", (req, res, next) => {
-  User.find({ username: req.body.username })
-    .exec()
-    .then(user => {
-      if (user.length >= 1) {
-        return res.status(409).json({
-          message: "User already exists."
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              firstName: req.body.firstName,
-              lastName: req.body.lastName,
-              email: req.body.email,
-              username: req.body.username,
-              password: hash
-            });
-            user
-              .save()
-              .then(result => {
-                console.log(result);
-                res.status(201).json({
-                  _id: result._id,
-                  message: "User created"
-                });
-              })
-              .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                  error: err
-                });
-              });
-          }
-        });
-      }
-    });
-});
-
-// Route to return ALL expenses in the database for a specific user.
-expenseRoutes.get("/getAllExpenses", (req, res, next) => {
-  //const userId = "5c78ce86a484a23550339d6a";
-  Expense.find({userId: userId}, function(err, expenses) {
-	
-	if (err) {
-		console.log(err);
-	} else {
-		res.json(expenses);
-	}
-  });
-});
-
-// Route to return all expenses for a specific month
-expenseRoutes.get("/month/:newMonth", (req, res, next) => {
-  //const userId = "5c78ce86a484a23550339d6a";
-  const month = req.params.newMonth;
-  console.log(month);
-  Expense.find({userId: userId, month: month}, function(err, expenses) {
-	console.log(expenses);
-	if (err) {
-		console.log(err);
-	} else {
-		res.json(expenses);
-	}
-  });
-});
-
-// Route to return all expenses with a specific group code
-expenseRoutes.get("/code/:thisCode", (req, res, next) => {
-  const groupCode = req.params.thisCode;
-  console.log(groupCode);
-  Expense.find({groupCode: groupCode}, function(err, expenses) {
-	console.log(expenses);
-	if (err) {
-		console.log(err);
-	} else {
-		res.json(expenses);
-	}
-  });
-});
-
-// Route to return specific expense in database.
-expenseRoutes.route('/:id').get(function(req, res) {
-    let id = req.params.id;
-    Expense.findById(id, function(err, expense) {
-        res.json(expense);
-    });
-});
-
-// Route to add expense 
-expenseRoutes.route('/add').post(function(req, res) {
-    let expense = new Expense(req.body);
-    expense.save()
-        .then(expense => {
-            res.status(200).json({'expense': 'expense added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('adding new expense failed');
-        });
-});
-
-expenseRoutes.route('/update/:id').post(function(req, res) {
-    Expense.findById(req.params.id, function(err, expense) {
-        if (!expense)
-            res.status(404).send("data is not found");
-        else
-            expense.description = req.body.description;
-            expense.amount = req.body.amount;
-            expense.month = req.body.month;
-            expense.day = req.body.day;
-            expense.year = req.body.year;
-            expense.groupCode = req.body.groupCode;
-            expense.save().then(expense => {
-                res.json('Expense updated!');
-            })
-            .catch(err => {
-                res.status(400).send("Update not possible");
-            });
-    });
-});
-
-// Route to delete
-expenseRoutes.delete("/delete/:id", (req, res, next) => {
-  //const userId = "5c78ce86a484a23550339d6a";
-  const id = req.params.id;
-  Expense.findOneAndDelete({_id: id}, function(err, expenses) {
-	if (err) {
-		console.log(err);
-	} else {
-		res.json({
-		success: id});
-	}
-  });
-});
-
-app.use('/expenses', expenseRoutes);
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-});
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
