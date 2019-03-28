e989a8bc0f580c5fc487757c20dbd9456bf64c41
Luke Hazelton <tomluke600@gmail.com>
2019-03-28 18:21:13 -0400

    Heroku deployment fix??

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const cors = require('cors');
 const path = require("path");
+const dotenv = require("dotenv").config();
 
 const users = require("./routes/api/users");
 
 const app = express();
 
 // Bodyparser middleware
+app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -37,16 +41,10 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
-
 const port = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
