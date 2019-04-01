ac18dc2b05c2cf31e01b521d8dc4867064baa637
Luke Hazelton <tomluke600@gmail.com>
2019-03-31 20:32:39 -0400

    Log/Reg Merge 5

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require("passport");
 const cors = require('cors');
-const mongoose = require('mongoose');
-const expenseRoutes = express.Router();
-const PORT = process.env.PORT || 4000; // "process.env.PORT" is Heroku's port if we're deploying there, then 4000 is a custom chosen port for dev testing
 const path = require("path");
 const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const app = express();
+
+const expenseRoutes = express.Router();
 
 let Expense = require('./models/expense');
 let User = require('./models/user');
 
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-
-
+// Bodyparser middleware
 app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client", "build")))
-mongoose.connect('mongodb://Admin:Hello2@cluster0-shard-00-00-7dwwj.mongodb.net:27017,cluster0-shard-00-01-7dwwj.mongodb.net:27017,cluster0-shard-00-02-7dwwj.mongodb.net:27017/testLarge?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true });
-const connection = mongoose.connection;
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+	.connect(
+		db,
+		{ useNewUrlParser: true }
+		)
+		.then(() => console.log("MongoDB successfully connected"))
+		.catch(err => console.log(err));
+/* const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
-})
+}) */
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// -----ROUTES-----
 
 // For mobile testing
 expenseRoutes.post('/all', (req, res, next) => {
@@ -248,7 +272,12 @@ expenseRoutes.delete("/delete/:id", (req, res, next) => {
   });
 });
 
+// -----END ROUTES-----
+
 app.use('/expenses', expenseRoutes);
+
+const PORT = process.env.PORT || 4000; // "process.env.PORT" is Heroku's port if we're deploying there, then 4000 is a custom chosen port for dev testing
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
