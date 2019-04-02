9ab32a746c352219fe3cda6d919f22552effdb72
Luke Hazelton <tomluke600@gmail.com>
2019-04-02 12:39:30 -0400

    Log/Reg Merge 19

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ require("./config/passport")(passport);
 
 // -----ROUTES-----
 
+const users = require("./routes/api/users");
+
 // For mobile testing
 expenseRoutes.post('/all', (req, res, next) => {
   const userId = req.body.userId;
@@ -271,6 +273,7 @@ expenseRoutes.delete("/delete/:id", (req, res, next) => {
 // -----END ROUTES-----
 
 app.use('/expenses', expenseRoutes);
+app.use("/api/users", users);
 
 const PORT = process.env.PORT || 4000; // "process.env.PORT" is Heroku's port if we're deploying there, then 4000 is a custom chosen port for dev testing
 
