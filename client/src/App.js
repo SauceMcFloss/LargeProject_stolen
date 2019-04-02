71311eff93a9d131a783319685257e8d858b2f7b
Luke Hazelton <tomluke600@gmail.com>
2019-04-02 10:08:12 -0400

    Log/Reg Merge 17

--- client/src/App.js
+++ client/src/App.js
@@ -50,38 +50,17 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <Router>
-        <div className="container">
-		<center><h1><font size = "10"><b>Track Dat CA$H</b></font></h1></center><br/>
-          <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <img src={logo} width="100" height="100" alt=""/>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">All Expenses</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Expense</Link>
-                </li>
-				<li className="navbar-item">
-                  <Link to="/monthly" className="nav-link">Monthly</Link>
-                </li>
-				<li className="navbar-item">
-                  <Link to="/group" className="nav-link">Group</Link>
-                </li>
-              </ul>
-            </div>
-			<img src={logo} width="100" height="100" alt=""/>
-          </nav>
-          <br/>
-		  <Route path="/register" exact component={CreateUser} />
-		  <Route path="/" exact component={ExpensesList} />
-          <Route path="/create" component={CreateExpense} />
-		  <Route path="/edit/:id" component={EditExpense} />
-		  <Route path="/monthly" component={Monthly} />
-		  <Route path="/group" component={Group} />
-        </div>
-      </Router>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </Router>
       </Provider>
     );
   }
