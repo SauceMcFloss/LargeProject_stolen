fd6450e5e243d51a02b95b0f55225621a60713eb
Luke Hazelton <tomluke600@gmail.com>
2019-04-03 01:08:24 -0400

    Log/Reg Merge 25

--- server.js
+++ server.js
@@ -274,6 +274,44 @@ expenseRoutes.delete("/delete/:id", (req, res, next) => {
   });
 });
 
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+expenseRoutes.post("/register", (req, res) => {
+  // Form validation
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
 app.use('/expenses', expenseRoutes);
 
 // -----END ROUTES----- 
