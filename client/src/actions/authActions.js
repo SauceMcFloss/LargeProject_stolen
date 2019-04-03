23a9e429c48315463b929189e2310a0fc5b8d352
Luke Hazelton <tomluke600@gmail.com>
2019-04-03 01:03:17 -0400

    Log/Reg Merge 24

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -8,7 +8,7 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 export const registerUser = (userData, history) => dispatch => {
 	console.log("We made it to register, at least.");
   axios
-    .post("/api/users/register", userData)
+    .post("/expenses/register", userData)
     .then(res => history.push("/login"))
     .catch(err =>
       dispatch({
