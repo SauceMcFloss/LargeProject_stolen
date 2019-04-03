fef9c2d17fdfdf5e37287b62f7647ce4ac258aad
Luke Hazelton <tomluke600@gmail.com>
2019-04-03 16:25:12 -0400

    Revert "Fix"
    This reverts commit 1ecbbf390bb25e74a403bd8be52e836dd6fe41f3.

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ expenseRoutes.post("/createUser", (req, res, next) => {
 });
 
 // Route to return ALL expenses in the database for a specific user.
-expenseRoutes.post("/getAllExpenses", (req, res, next) => {
+expenseRoutes.get("/getAllExpenses", (req, res, next) => {
   const userId = "5c78ce86a484a23550339d6a";
   Expense.find({userId: userId}, function(err, expenses) {
 	
