ef212c59e94a6ca742ee7ba7cb3d1644cec1e458
Luke Hazelton <tomluke600@gmail.com>
2019-04-02 23:49:35 -0400

    Revert "Log/Reg Merge 21"
    This reverts commit b275fca0e28845b34f313484ea94f40da4883988.

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const path = require("path");
 const dotenv = require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const keys = require("../../config/keys");
-
-// Load input validation
-const validateRegisterInput = require("./validation/register");
-const validateLoginInput = require("./validation/login");
 
 const app = express();
 
@@ -48,9 +43,9 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
-// -----ROUTES-----
+/* // -----ROUTES-----
 
-/* // For mobile testing
+// For mobile testing
 expenseRoutes.post('/all', (req, res, next) => {
   const userId = req.body.userId;
   Expense.find({userId: userId})
@@ -273,67 +268,9 @@ expenseRoutes.delete("/delete/:id", (req, res, next) => {
   });
 });
 
-app.use('/expenses', expenseRoutes); */
+app.use('/expenses', expenseRoutes);
 
-// @route POST api/users/login
-// @desc Login user and return JWT token
-// @access Public
-router.post("/login", (req, res) => {
-  // Form validation
-
-  const { errors, isValid } = validateLoginInput(req.body);
-
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const email = req.body.email;
-  const password = req.body.password;
-
-  // Find user by email
-  User.findOne({ email }).then(user => {
-    // Check if user exists
-    if (!user) {
-      return res.status(404).json({ emailnotfound: "Email not found" });
-    }
-
-    // Check password
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name
-        };
-
-        // Sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ passwordincorrect: "Password incorrect" });
-      }
-    });
-  });
-});
-
-app.use("/api/users", users);
-
-// -----END ROUTES-----
+// -----END ROUTES----- */
 
 const PORT = process.env.PORT || 4000; // "process.env.PORT" is Heroku's port if we're deploying there, then 4000 is a custom chosen port for dev testing
 
