4f9f872832be0825e0e635a8a5542a4032678cd6
Luke Hazelton <tomluke600@gmail.com>
2019-04-03 19:55:25 -0400

    fix logging

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ expenseRoutes.post("/createUser", (req, res, next) => {
 
 // Route to return ALL expenses in the database for a specific user.
 expenseRoutes.post("/getAllExpenses", (req, res, next) => {
-	console.log("req.body: " + req.body);
+	console.log(req.body);
   Expense.find({userId: req.body}, function(err, expenses) {
 	
 	if (err) {
