8117a2132b6bc9187ce87aca4c0e39baf8f9642a
Luke Hazelton <tomluke600@gmail.com>
2019-04-03 23:14:10 -0400

    Dynamic UserId!?

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ expenseRoutes.post("/createUser", (req, res, next) => {
 expenseRoutes.post("/getAllExpenses", (req, res, next) => {
 	const userId = req.body.toString();
 	console.log(userId);
-  Expense.find({userId: "5c9945edf5c8de55c0564c63"}, function(err, expenses) {
+  Expense.find({userId: userId}, function(err, expenses) {
 	
 	if (err) {
 		console.log(err);
