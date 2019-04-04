108d83c88bfe162eee92be8d836362d6dee7cffe
Luke Hazelton <tomluke600@gmail.com>
2019-04-04 02:34:55 -0400

    test change

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ expenseRoutes.post("/createUser", (req, res, next) => {
 });
 
 // Route to return ALL expenses in the database for a specific user.
-expenseRoutes.post("/getAllExpenses", (req, res, next) => {
+expenseRoutes.route("/getAllExpenses").post(function(req, res) {
 	const usersId = req.body.toString();
   Expense.find({userId: "5c9945edf5c8de55c0564c63"}, function(err, expenses) {
 	 
